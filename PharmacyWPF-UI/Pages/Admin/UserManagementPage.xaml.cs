5a9becd1db172d3e561a0c653984c04045422cf0
Talal <Talal@TALAL>
2026-05-15 17:29:37 +0300

    Structuring the ui framework

---
+++ PharmacyWPF-UI/Pages/Admin/UserManagementPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PharmacyWPF_UI.Pages.Admin
+{
+    /// <summary>
+    /// Interaction logic for UserManagementPage.xaml
+    /// </summary>
+    public partial class UserManagementPage : Page
+    {
+        public UserManagementPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
